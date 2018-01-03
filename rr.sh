e82007b634470bbde365293891584497e46f802c
Eryk Napierala <eryk.napierala@allegrogroup.com>
2018-01-03 21:35:09 +0100

    Add release & run script

---
+++ rr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "==================="
+echo "== Release & Run =="
+echo "==================="
+
+echo "= Build ==========="
+npm run build
+
+echo "= Release ========="
+npm run release
+
+echo "= Copying package ="
+mkdir -p ~/release
+cp release.zip ~/release
+cd ~/release
+
+echo "= Extracting ====="
+unzip -o release.zip
+
+echo "= Installing ====="
+yarn
+
+echo "= Starting ======="
+npm start
